fd72c2d0e6bae769b424948d555dc599e4dde919
Ashley (Skye) Brown <stormi.seuneu@outlook.com>
2022-05-27 15:17:13 -0500

    added search logic

--- fetch.js
+++ fetch.js
@@ -1,13 +1,24 @@
-fetch("https://itunes.apple.com/search?term=$%7BMadonna%7D&media=music&entity=album&attribute=artistTerm&limit=200%22")
-  .then(res => res.json())
-  .then(data => addData(data.results));
+const searchInput = document.querySelector('[album-search');
 
-const addData = data => {
+searchInput.addEventListener('input', e => {
+  const value = e.target.value;
+  if (!value) window.alert('Must type something')
+  try {
+    fetch(`https://itunes.apple.com/search?term=$%7B${value}%7D&media=music&entity=album&attribute=artistTerm&limit=200%22`)
+      .then(res => res.json())
+      .then(data => addData(value ,data.results));
+  } catch(e) {
+    window.alert('Must have vaild search')
+  }
+});
+
+
+const addData = (value, data) => {
   console.log(data);
   let resultCount = data.length;
   let resultTitle = document.querySelector('#intro');
 
-  resultTitle.innerText = `${resultCount} results for 'artist'`
+  resultTitle.innerText = `${resultCount} results for ${value}`
 
   data.forEach(album => {
     let resultContainer = document.querySelector('#result-container');
